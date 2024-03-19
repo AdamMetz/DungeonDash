ca3020a9e363ce68c65d03896a14946004bd2d33
AdamMetz <47069058+AdamMetz@users.noreply.github.com>
2024-03-19 16:21:04 -0600

    Add player movement animation

--- src/DungeonDash/Assets/Scripts/PlayerMovement.cs
+++ src/DungeonDash/Assets/Scripts/PlayerMovement.cs
@@ -7,15 +7,23 @@ public class PlayerMovement : MonoBehaviour
 {
     private Vector2 playerDirection;
     private Rigidbody2D rb;
+    private Animator animator;
 
     public int speed = 3;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     private void OnMovement(InputValue inputValue) { 
         playerDirection = inputValue.Get<Vector2>();
+
+        if (playerDirection == Vector2.zero) { 
+            animator.SetBool("isMoving", false); 
+        } else { 
+            animator.SetBool("isMoving", true); 
+        }
     }
 
     private void FixedUpdate()
