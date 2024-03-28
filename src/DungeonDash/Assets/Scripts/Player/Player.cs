73124bda545fed82c0d90e520309cdbc1ad5139a
AdamMetz <47069058+AdamMetz@users.noreply.github.com>
2024-03-28 15:59:29 -0600

    Add player spawn points for each room

--- src/DungeonDash/Assets/Scripts/Player/Player.cs
+++ src/DungeonDash/Assets/Scripts/Player/Player.cs
@@ -13,14 +13,30 @@ public class Player : MonoBehaviour
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode) 
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         if (scene.name.Contains("Room"))
         {
             DontDestroyOnLoad(gameObject);
-        } else
+            SpawnPlayer();
+        }
+        else
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void SpawnPlayer()
+    {
+        GameObject playerSpawnPoint = GameObject.Find("PlayerSpawnPoint");
+
+        if (playerSpawnPoint != null)
+        {
+            transform.position = new Vector3(playerSpawnPoint.transform.position.x, playerSpawnPoint.transform.position.y, transform.position.z);
+        }
+        else
+        {
+            Debug.LogError("PlayerSpawnPoint not found in the scene.");
         }
     }
 }
