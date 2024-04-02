f61b5bc9538647d6bdbb859562a0ccb380974dc9
AdamMetz <47069058+AdamMetz@users.noreply.github.com>
2024-04-02 16:31:30 -0600

    Add CharacterSelect menu functionality

---
+++ src/DungeonDash/Assets/Scripts/Menu/CharacterSelect.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelect : MonoBehaviour
+{
+    public void Archer()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Mage()
+    {
+        // SceneManager.LoadScene(20);
+    }
+}
