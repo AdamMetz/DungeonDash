70ecb6df2a5ed43797cc2c4ced7c89173070aa01
AdamMetz <47069058+AdamMetz@users.noreply.github.com>
2024-04-02 15:44:38 -0600

    Add final boss object

--- src/DungeonDash/Assets/PlayerSpawnPoint.cs
+++ src/DungeonDash/Assets/PlayerSpawnPoint.cs
@@ -6,7 +6,6 @@ public class PlayerSpawnPoint : MonoBehaviour
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        print(player);
         player.transform.position = new Vector3(transform.position.x, transform.position.y, 0);
     }
 }
