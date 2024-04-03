6e9ad9101482579b66074f62b4f0f6f09c86db68
AdamMetz <47069058+AdamMetz@users.noreply.github.com>
2024-04-02 19:22:39 -0600

    HealthPotion and Item collision bug fixes

--- src/DungeonDash/Assets/Scripts/Items/HealthPotion.cs
+++ src/DungeonDash/Assets/Scripts/Items/HealthPotion.cs
@@ -4,7 +4,7 @@ public class HealthPotion : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Player") {
+        if (collision.gameObject.tag == "Player") {
             CharacterHealth playerHealth = collision.gameObject.GetComponent<CharacterHealth>();
             if (playerHealth.Heal(1)) Destroy(gameObject);
         }
